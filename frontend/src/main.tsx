d768d98fb224d207e818c9e3cfcd6cd170dc23a8
Florencia Pons <ponsmariaflorencia@gmail.com>
2025-10-01 21:39:14 -0300

    feat: add frontend Docker setup with Nginx and environment support

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 import './index.css'
 import App from './App'
 
@@ -7,7 +9,9 @@ const rootElement = document.getElementById('root')
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </StrictMode>
   )
 }
