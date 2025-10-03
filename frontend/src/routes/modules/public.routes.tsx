615c120f5b17502a10e4c25962e7edc6486743cb
Florencia Pons <ponsmariaflorencia@gmail.com>
2025-10-02 21:36:09 -0300

    feat(routes): Create public and private routes with auth protection. Add Routes map #13. Normalize line endings

---
+++ frontend/src/routes/modules/public.routes.tsx
@@ -0,0 +1,9 @@
+import Login from '../../pages/Login';
+import Register from '../../pages/Register';
+import Home from '../../pages/Home';
+
+export const PublicRoutes = [
+  { path: '/', element: <Home /> },
+  { path: '/login', element: <Login /> },
+  { path: '/register', element: <Register /> },
+];
