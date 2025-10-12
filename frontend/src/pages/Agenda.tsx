2b47f8f653851250bf7782ab95ca22a569691a1d
Florencia Pons <ponsmariaflorencia@gmail.com>
2025-10-11 23:49:50 -0300

    Pages: component gallery and base page creation#8

---
+++ frontend/src/pages/Agenda.tsx
@@ -0,0 +1,5 @@
+const Agenda = () => {
+  return <div>Agenda</div>;
+};
+
+export default Agenda;
---
+++ frontend/src/pages/professionals.tsx
@@ -0,0 +1,10 @@
+const Professionals = () => {
+  return <div>Professionals</div>;
+  <section id="cardiologia" className="scroll-mt-20">
+  <h2 className="text-2xl font-bold mb-4">Cardiología</h2>
+  {/* contenido */}
+</section>
+
+};
+
+export default Professionals;
