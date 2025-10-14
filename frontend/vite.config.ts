c39f70432096de23fbb12c8cce2f7e2cad5bbf0e
Florencia Pons <ponsmariaflorencia@gmail.com>
2025-10-14 11:01:45 -0300

    chore: proxy configuration in Vite to avoid CORS blocking when accessing the remote backend from the local frontend.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,16 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.mbst.online',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      
+    },
+  },
 });
