8b4aa39f8f8dd6ee75e939b013be0c0c9af60014
Florencia Pons <ponsmariaflorencia@gmail.com>
2025-10-18 04:07:42 -0300

    Modularization: login schema, separation of APIs, and update auth
    Modularization: login schema for validation with Zod, separation of APIs
    for instantiated Axios and update of auth store
    
    Modularization: login schema for validation with Zod, separation of
    APIs  for instantiated Axios and update of auth store
    :wq

---
+++ frontend/src/store/useAuthStore.tsx
@@ -0,0 +1,84 @@
+import { create } from 'zustand';
+import { fetchUser, logoutUser } from '../services/auth';
+import type { User } from '../types/user.types';
+ 
+interface AuthState {
+  user: User | null;
+  role: User['role'] | null;
+  token: string | null;
+  isAuthenticated: boolean;
+  isLoading: boolean;
+  error: string | null;
+  setUser: (user: User) => void;
+  setToken: (token: string) => void;
+  logout: () => void;
+  validateSession: () => Promise<void>;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  role: null,
+  token: localStorage.getItem('token'),
+  isAuthenticated: false,
+  isLoading: false,
+  error: null,
+
+  setUser: (user) =>
+    set({
+      user,
+      role: user.role,
+      isAuthenticated: true,
+    }),
+
+  setToken: (token) =>
+    set({
+      token,
+      isAuthenticated: true,
+    }),
+
+  logout: async () => {
+    try {
+      await logoutUser();
+    } catch (error) {
+      console.warn("Logout fallido:", error);
+    }
+
+    localStorage.removeItem('token');
+    set({
+      user: null,
+      role: null,
+      token: null,
+      isAuthenticated: false,
+      isLoading: false,
+      error: null,
+    });
+  },
+
+
+  validateSession: async () => {
+    const token = localStorage.getItem('token');
+    if (!token) return set({ isAuthenticated: false, isLoading: false });
+
+    set({ isLoading: true });
+    try {
+      const user = await fetchUser();
+      set({
+        user,
+        role: user.role,
+        token,
+        isAuthenticated: true,
+        isLoading: false,
+      });
+    } catch {
+      localStorage.removeItem('token');
+      set({
+        user: null,
+        role: null,
+        token: null,
+        isAuthenticated: false,
+        isLoading: false,
+      });
+    }
+  },
+}));
+
